e7917ee9c86bbcf44f02b425caff27910aaf0ed0
Abhinand <abhin4nd6@gmail.com>
2025-01-28 22:06:04 +0530

    fix: fix in server

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,7 @@ import { ENV } from './constants/env';
 import express from 'express'
 import { connectDB } from './config/db';
 import cors from 'cors'
+import { errorHandler } from './middlewares/errorMiddlware';
 const app = express()
 
 app.use(express.json());
@@ -13,8 +14,24 @@ app.use(cors({
     credentials: true // Allow cookies to be sent with requests
 }))
 app.use('/api', router)
+app.use(errorHandler)
+let intervalId:any;
+const fetchApiData = async () => {
+    try {
+        const response = await fetch("https://mamo.onrender.com/api/timeslots");
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        console.log("API Response:", response.status);
+    } catch (error) {
+        console.error("Error fetching API data:", error);
+    }
+};
+
+intervalId = setInterval(fetchApiData, 14000);
 
 app.listen(ENV.PORT, async () => {
     await connectDB()
     console.log('Server is listening on port:', ENV.PORT)
-})
+}) 
