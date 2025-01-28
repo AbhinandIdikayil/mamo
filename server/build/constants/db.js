7a7f7da2b18d6cdcccea0c34fcfdbe404d7811d4
Abhinand <abhin4nd6@gmail.com>
2025-01-28 20:23:24 +0530

    fix: removed build folder

--- server/build/constants/db.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DB_enum = void 0;
-var DB_enum;
-(function (DB_enum) {
-    DB_enum["HR_MODEL"] = "Hrs";
-    DB_enum["INTERVIEW_MODEL"] = "Interview";
-})(DB_enum || (exports.DB_enum = DB_enum = {}));
--- server/build/utils/success.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.success = void 0;
-const success = (res, { data = {}, message = 'success', status = 200 }) => {
-    return res.status(status).json({ message, data });
-};
-exports.success = success;
